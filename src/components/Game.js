75880c88d07efd6256cc6a2c4289415df2d8b95b
codershona <falgunimst95@gmail.com>
2020-08-15 14:25:07 +0200

    working on game

---
+++ src/components/Game.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import Square from './Square';
+
+
+
+
+
+
+
+export default class Game extends Component {
+
+	constructor(props) {
+
+		super(props);
+		this.state = {
+			xIsNext: true
+		}
+
+	}
+
+
+	render() {
+
+		return (
+
+			<div>
+
+
+
+			
+			</div>
+
+
+
+			)
+	}
+}
