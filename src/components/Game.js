e9dfb7f8e03d9cc1facc6978f5b36036089d1c3c
codershona <falgunimst95@gmail.com>
2020-08-15 14:57:10 +0200

    working on calculate winners in component board

--- src/components/Game.js
+++ src/components/Game.js
@@ -36,7 +36,7 @@ export default class Game extends Component {
 
     const squares = current.squares.slice();
 
-    
+  
 
 
 
@@ -90,3 +90,35 @@ export default class Game extends Component {
 			)
 	}
 }
+
+
+
+  {/* calculate winners in component board   */} 
+
+
+
+  function calculateWinner(squares) {
+
+  	const lines = [
+
+    	[0, 1, 2],
+    	[3, 4, 5],
+    	[6, 7, 8],
+    	[0, 3, 6],
+    	[1, 4, 7],
+    	[2, 5, 8],
+    	[0, 4, 8],
+    	[2, 4, 6]
+
+
+
+
+  	];
+
+
+   
+
+
+  	return null;
+
+  }
