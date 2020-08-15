18c20a3991b8a01979c219adf09d1da6ef68f921
codershona <falgunimst95@gmail.com>
2020-08-15 14:09:37 +0200

    creating component of game

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,16 +4,18 @@ import React from 'react';
 
 
 
-export default function Square() {
+
+
+export default function Square(props) {
 
 	return (
 
-		<div>
+		<button className="square" onClick={props.onClick}>
+
+            {props.value}
 
 
-
-
-		</div>
+		</button>
 
 
 
